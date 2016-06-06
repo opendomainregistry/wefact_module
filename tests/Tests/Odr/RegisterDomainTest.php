62195c7fed3257db612a8358125af793f9894052
staticall <staticall@gmail.com>
2016-06-06 20:15:53 +0700

    Tests: Register and transfer now testable with nameservers

--- tests/Tests/Odr/RegisterDomainTest.php
+++ tests/Tests/Odr/RegisterDomainTest.php
@@ -147,4 +147,34 @@ class RegisterDomainTest extends UnitTestCase
 
         self::assertTrue($wefact->registerDomain('test.nl', array(), $whois));
     }
+
+    public function testSuccessWithNameservers()
+    {
+        $wefact = $this->getModule();
+
+        $wefact->odr->setConfig(
+            array(
+                'api_key'    => 'public$success',
+                'api_secret' => 'secret$success',
+                'token'      => 'token$success',
+                'url'        => $wefact::URL_TEST,
+            )
+        );
+
+        $whois = new Whois;
+
+        $whois->ownerRegistrarHandles = array(
+            'opendomainregistry' => 1,
+        );
+
+        $whois->adminRegistrarHandles = array(
+            'opendomainregistry' => 1,
+        );
+
+        $whois->techRegistrarHandles = array(
+            'opendomainregistry' => 1,
+        );
+
+        self::assertTrue($wefact->registerDomain('test.nl', array('ns1' => 'ns1.test.ru', 'ns2' => 'ns2.test.ru'), $whois));
+    }
 }
--- tests/Tests/Odr/TransferDomainTest.php
+++ tests/Tests/Odr/TransferDomainTest.php
@@ -147,4 +147,34 @@ class TransferDomainTest extends UnitTestCase
 
         self::assertTrue($wefact->transferDomain('test.nl', array(), $whois));
     }
+
+    public function testSuccessWithNameservers()
+    {
+        $wefact = $this->getModule();
+
+        $wefact->odr->setConfig(
+            array(
+                'api_key'    => 'public$success',
+                'api_secret' => 'secret$success',
+                'token'      => 'token$success',
+                'url'        => $wefact::URL_TEST,
+            )
+        );
+
+        $whois = new Whois;
+
+        $whois->ownerRegistrarHandles = array(
+            'opendomainregistry' => 1,
+        );
+
+        $whois->adminRegistrarHandles = array(
+            'opendomainregistry' => 1,
+        );
+
+        $whois->techRegistrarHandles = array(
+            'opendomainregistry' => 1,
+        );
+
+        self::assertTrue($wefact->transferDomain('test.nl', array('ns1' => 'ns1.test.ru', 'ns2' => 'ns2.test.ru'), $whois));
+    }
 }
